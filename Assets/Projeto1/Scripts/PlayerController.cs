47b9943b72f41952028673090ca091353fabead6
ginger59620 <a59620@alunos.ipb.pt>
2025-05-25 13:11:51 +0100

    New thing in the project 1

--- Assets/Projeto1/Scripts/PlayerController.cs
+++ Assets/Projeto1/Scripts/PlayerController.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //private variable
-    private float speed = 5.0f;
-    private float turnSpeed = 25.0f;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //player input
         horizontalInput = Input.GetAxis("Horizontal");
